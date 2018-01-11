fe408c32a753cfceea21921a1a121ddc80041158
Taylor Growden <taylorgrowden@gmail.com>
2018-01-11 13:34:27 -0500

    fix price formatting

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,8 +2,9 @@ const getSymbolFromCurrency = require('currency-symbol-map')
 
 module.exports = {
     formatPrice: (price, currency) => {
+        const formattedPrice = price.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+        if (!currency) return formattedPrice
         const symbol = getSymbolFromCurrency(currency)
-        const formattedPrice = price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
         if (typeof symbol === 'undefined') {
             return `${formattedPrice} ${currency}`
         }
